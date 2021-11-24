7d4c9bc0f2bec01efaf18bf69948d1e0b016dc55
Kris-Furer <krisFurer@gmail.com>
2021-11-24 15:30:14 +1300

    resolving conflict

--- front-page.php
+++ front-page.php
@@ -3,6 +3,7 @@
   <div class="container-fluid">
     <!-- Landing Section -->
     <div class="row section mb-5">
+<<<<<<< Updated upstream
       <!-- Landing Text -->
       <div class="col-sm-12 col-md-5 d-flex align-items-center text-section order-sm-2">
         <div class="px-md-5 my-md-5">
@@ -11,13 +12,24 @@
           <span class='my-btn' href="#">Enquire now</span>
         </div>
       </div>
+=======
+
+>>>>>>> Stashed changes
       <!-- Landing Image -->
-      <div class="col-sm-12 col-md-7 p-md-0 order-sm-1 order-md-2 image-section">
+      <div class="col-sm-12 col-md-7 p-md-0  image-section order-md-2">
           <?php if (get_theme_mod( 'hero_image' )) { ?>
               <img class="hero-img" src="<?php echo get_theme_mod( 'hero_image')?>" alt=""> <?php } else { ?>
-
               <img class="section-img" src="<?php echo get_template_directory_uri().'/images/hero.png'?>" alt="">
-              <?php } ?>
+          <?php } ?>
+      </div>
+
+      <!-- Landing Text -->
+      <div class="col-sm-12 col-md-5 d-flex align-items-center ">
+        <div class="px-md-5 my-md-5">
+          <h1 class="display-1 mb-0 mt-3"><?php echo get_theme_mod('call_to_action')?></span> </h1>
+          <p class="py-3 lead"><?php echo get_theme_mod('subhead_text')?></p>
+          <span class='my-btn' href="#">Enquire now</span>
+        </div>
       </div>
     </div>
 
@@ -25,15 +37,12 @@
     <!-- Section 2 Starts -->
     <div class="row section my-5">
       <div class="col-sm-12 col-md-7 ps-md-0 image-section">
-
         <img class="section-img" src="<?php if (get_theme_mod( 'section_2_image' )) : echo get_theme_mod( 'section_2_image');
         else: echo get_template_directory_uri().'/images/picture2.png'; endif; ?>" alt="">
-
       </div>
-
-      <div class="col-sm-12 col-md-4 d-flex align-items-center text-section">
+      <div class="col-sm-12 col-md-4 d-flex align-items-center ">
         <div class="ps-md-5">
-          <h2 class="display-3">Building<span class="font-italic"> your</span> dream house</h2>
+          <h2 class="display-3 mt-3">Building<span class="font-italic"> your</span> dream house</h2>
           <p><?php echo get_theme_mod('section2_text') ?>
           </p>
         </div>
@@ -44,19 +53,21 @@
 
 <!-- Section 3 Starts  -->
 <div class="row section my-5">
+  <!-- Section 3 Image -->
+  <div class="col-sm-12 col-md-6 pe-md-0 image-section order-md-2">
+    <img class="section-img" src="<?php if (get_theme_mod( 'section_3_image' )) : echo get_theme_mod( 'section_3_image');
+  else: echo get_template_directory_uri().'/images/picture-3.png'; endif; ?>" alt="">
+  </div>
+
   <!-- section 2 Image -->
-  <div class="col-sm-12 col-md-6 d-flex align-items-center text-section order-sm-2">
+  <div class="col-sm-12 col-md-6 d-flex align-items-center">
     <div class="p-md-5">
-      <h2 class='display-3'>Quality builders, quality<span class="font-italic"> homes</span></h2>
+      <h2 class='display-3 mt-3'>Quality builders, quality<span class="font-italic"> homes</span></h2>
       <p class='py-2'><?php echo get_theme_mod('section3_text') ?></p>
       <a class='my-btn' href="#">Enquire now</a>
     </div>
   </div>
-  <!-- Section 3 Image -->
-  <div class="col-sm-12 col-md-6 pe-md-0 order-sm-1 order-md-2 image-section">
-    <img class="section-img" src="<?php if (get_theme_mod( 'section_3_image' )) : echo get_theme_mod( 'section_3_image');
-  else: echo get_template_directory_uri().'/images/picture-3.png'; endif; ?>" alt="">
-  </div>
+
 </div>
 
 <!-- Testimonial Section  -->
